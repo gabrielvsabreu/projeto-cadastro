c57ff60281f46731ed6ba17c9774636a5c9fbe9a
Gabriel Abreu <gabrielvsabreu@gmail.com>
2023-01-10 01:42:48 -0300

    Atualizações vídeo parte 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ class Validator {
 
     constructor() {
         this.validations = [
+
+            'data-required', 
             'data-min-length', 
+            'data-max-length', 
+            'data-email-validate',
+            'data-only-letters', 
+            'data-equal', 
+            'data-password-validate', 
+
         ]
     }
 
@@ -10,11 +18,19 @@ class Validator {
 
     validate(form) {
 
-        // pegar os inputs
+        // Resgata todas as validações
+
+        let currentValidations = document.querySelectorAll('form .error-validation');
+
+        if(currentValidations.length > 0) {
+            this.cleanValidations(currentValidations);
+        }
+
+        // Pega os inputs
 
         let inputs = form.getElementsByTagName('input');        
 
-        // Transformar HTMLCollection --> array
+        // Transforma HTMLCollection --> array
 
         let inputsArray = [...inputs];
         
@@ -30,7 +46,7 @@ class Validator {
 
                 if(input.getAttribute(this.validations[i]) != null) {
                     
-                    // Limpar a string para virar um método
+                    // Limpa a string para virar um método
 
                     let method = this.validations[i].replace('data-', '').replace('-', '');
 
@@ -38,7 +54,7 @@ class Validator {
 
                     let value = input.getAttribute(this.validations[i]);
 
-                    // Chamar o método
+                    // Chama o método
 
                     this[method](input, value);
 
@@ -64,28 +80,159 @@ class Validator {
 
     }
 
+    // Verifica se o input excedeu o limite de caracteres
+
+    maxlength(input, maxValue) {
+
+        let inputLength = input.value.length;
+
+        let errorMessage = `O campo pode conter no máximo ${maxValue} caracteres.`;
+
+        if(inputLength > maxValue) {
+            this.printMessage(input, errorMessage);
+        }
+
+    }
+
+    // Validação de e-mails
+
+    emailvalidate(input) {
+
+        let re = /\S+@\S+\.\S+/;
+
+        let email = input.value;
+
+        let errorMessage = `Insira um e-mail no padrão email@email.com`;
+
+        if(!re.test(email)) {
+            this.printMessage(input, errorMessage);
+        }
+
+    }
+
+    // Valida se o campo recebeu apenas letras
+
+    onlyletters(input) {
+
+        let re = /^[A-Za-z]+$/;
+
+        let inputValue = input.value;
+
+        let errorMessage = `Este campo aceita apenas letras.`;
+
+        if(!re.test(inputValue)) {
+            this.printMessage(input, errorMessage);
+        }
+
+    }
+
     // Método para imprimir as mensagens de erro na tela
 
     printMessage(input, msg) {
 
-        let template = document.querySelector('.error-validation').cloneNode(true);
+        // Verifica a quantidade de erros que o input possui
 
-        template.textContent = msg;
+        let errorsQty = input.parentNode.querySelector('.error-validation');
 
-        let inputParent = input.parentNode;
+        if(errorsQty === null) {
 
-        template.classList.remove('template');
+            let template = document.querySelector('.error-validation').cloneNode(true);
 
-        inputParent.appendChild(template);
+            template.textContent = msg;
+
+            let inputParent = input.parentNode;
+
+            template.classList.remove('template');
+
+            inputParent.appendChild(template);
+
+        }
 
 
-    } 
+    }
+
+    // Verifica se o input é requerido
+
+    required(input) {
+
+        let inputValue = input.value;
+
+        if(inputValue === '') {
+           let errorMessage = `Este campo é obrigatório.`;
+
+           this.printMessage(input, errorMessage);
+        }
+
+    }
+
+    // Verifica se dois campos receberam valores iguais
+
+    equal(input, inputName) {
+
+        let inputToCompare = document.getElementsByName(inputName)[0];
+
+        let errorMessage = `Este campo e o campo "Senha" precisam ser iguais.`;
+
+        if(input.value != inputToCompare.value) {
+
+            this.printMessage(input, errorMessage);
+
+        }
+
+    }
+
+    // Valida o campo de senha
+
+    passwordvalidate(input) {
+
+        // Transforma string em array
+
+        let charArr = input.value.split("");
+
+        let uppercases = 0;
+        let numbers = 0;
+
+        for(let i = 0; charArr.length > i; i++) {
+            
+            if(charArr[i] === charArr[i].toUpperCase() && isNaN(parseInt(charArr[i]))) {
+
+                uppercases++;
+
+            } else if(!isNaN(parseInt(charArr[i]))) {
+
+                numbers++;
+
+            }
+
+        }
+
+        if(uppercases === 0 || numbers === 0) {
+
+            let errorMessage = `A senha precisa conter ao menos 1 letra maiúscula e 1 número.`;
+
+            this.printMessage(input, errorMessage);
+            
+        }
+
+    }
+
+    // Limpa as validações da tela
+
+    cleanValidations(validations) {
+
+        validations.forEach(el => el.remove());
+
+    }
+
 }
 
 let form = document.getElementById("register-form");
+
 let submit = document.getElementById("btn-submit");
 
+
 let validator = new Validator();
+
 
 // Evento que dispara as validações
 
@@ -94,4 +241,5 @@ submit.addEventListener('click', function(e) {
     e.preventDefault();
 
     validator.validate(form);
+
 });
